67052bcb6757825414ee8b9af2c9ebe448538856
Nicolai Helldorff <nicolai.helldorff@gmail.com>
2021-07-15 21:24:12 +0200

    FIXED UI-bugs

--- src/main/java/com/xrpdev/xrpgenerator/Controller.java
+++ src/main/java/com/xrpdev/xrpgenerator/Controller.java
@@ -1,14 +1,15 @@
 package com.xrpdev.xrpgenerator;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import net.glxn.qrgen.QRCode;
 import net.glxn.qrgen.image.ImageType;
 import org.xrpl.xrpl4j.wallet.DefaultWalletFactory;
@@ -16,6 +17,9 @@ import org.xrpl.xrpl4j.wallet.SeedWalletGenerationResult;
 import org.xrpl.xrpl4j.wallet.Wallet;
 import org.xrpl.xrpl4j.wallet.WalletFactory;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.net.URL;
@@ -43,12 +47,16 @@ public class Controller implements Initializable {
     @FXML
     private ImageView iv_code;
 
+    @FXML
+    private Text text_loading;
+
     private static final WalletFactory walletFactory = DefaultWalletFactory.getInstance();
 
     public static final ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        text_loading.setVisible(false);
 
         tc_address.setCellValueFactory(new PropertyValueFactory<>("account"));
         tc_seed.setCellValueFactory(new PropertyValueFactory<>("secret"));
@@ -74,6 +82,7 @@ public class Controller implements Initializable {
                 }
             });
             btn_search.setDisable(true);
+            text_loading.setVisible(true);
         });
 
 
@@ -83,6 +92,14 @@ public class Controller implements Initializable {
 
                     Image image = new Image(in);
                     iv_code.setImage(image);
+
+                    StringSelection selection = new StringSelection(newValue.getSecret());
+                    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                    clipboard.setContents(selection, selection);
+
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("The family seed was copied to the clipboard");
+                    alert.show();
                 }
         );
     }
