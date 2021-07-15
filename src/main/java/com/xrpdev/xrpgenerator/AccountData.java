3a9a2480aaf0931c73a552b2a47885be720718ee
Nicolai Helldorff <nicolai.helldorff@gmail.com>
2021-07-15 21:52:17 +0200

    UPDATED Dependencies

--- src/main/java/com/xrpdev/xrpgenerator/AccountData.java
+++ src/main/java/com/xrpdev/xrpgenerator/AccountData.java
@@ -1,14 +1,28 @@
 package com.xrpdev.xrpgenerator;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class AccountData {
 
     private String account;
     private String secret;
 
+    public AccountData(String account, String secret) {
+        this.account = account;
+        this.secret = secret;
+    }
 
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
 }
